71591df29b7c5568fd374b64add46c4888270b45
RDUBPhoto <rdubphoto@gmail.com>
2020-02-04 23:12:56 -0600

    Pushing to DIST branch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <Navigation></Navigation>
-      <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import Navigation from '@/components/Navigation.vue' 
+import Navigation from '@/components/Navigation.vue';
 export default {
 
 components: {
